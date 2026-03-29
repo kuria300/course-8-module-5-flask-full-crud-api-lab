641612875c24d6628a1cf561378fa0685c7a62d3
Kuria300 <eugenekuria66@gmail.com>
2026-03-29 16:44:46 +0300

    chnages

--- app.py
+++ app.py
@@ -22,33 +22,63 @@ events = [
 @app.route("/events", methods=["POST"])
 def create_event():
     # TODO: Task 2 - Design and Develop the Code
+    data=request.get_json()
 
+    if not data or "title" not in data:
+        return jsonify({"error":"title missing"}), 400
+
+    title=data.get('title')
     # TODO: Task 3 - Implement the Loop and Process Each Element
+    result={
+        "id":len(events) + 1,
+        "title":f"{title}"
+    }
+    events.append(result)
+    
 
+    return jsonify(result), 201
     # TODO: Task 4 - Return and Handle Results
-    pass
+    
 
 # TODO: Task 1 - Define the Problem
 # Update the title of an existing event
 @app.route("/events/<int:event_id>", methods=["PATCH"])
 def update_event(event_id):
     # TODO: Task 2 - Design and Develop the Code
+    data=request.get_json()
 
-    # TODO: Task 3 - Implement the Loop and Process Each Element
+    if not data or "title" not in data:
+        return jsonify({"error":"Data missing"}), 400
 
-    # TODO: Task 4 - Return and Handle Results
-    pass
+    for event in events:
+        if event.id == event_id:
+            event.title=data.get('title')
+            
+        return jsonify({"title": event.title}), 200
 
+    return jsonify({"error": "event not found"}), 404
+
+    
 # TODO: Task 1 - Define the Problem
 # Remove an event from the list
 @app.route("/events/<int:event_id>", methods=["DELETE"])
 def delete_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
+    global events
+    deleted_event = None  # (e)
+    
+    for e in events:
+        if e.id == event_id:
+            deleted_event = e  # Fixed typo: changed 'a' to 'e'
+            break
 
-    # TODO: Task 3 - Implement the Loop and Process Each Element
+    if deleted_event is None:
+        return jsonify({"error": "event not found"}), 404
 
-    # TODO: Task 4 - Return and Handle Results
-    pass
+    events = [e for e in events if e.id != event_id]
+
+    return "", 204
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
